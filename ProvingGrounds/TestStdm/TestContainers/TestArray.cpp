4b2dcb12f4a3ecd29443ed16d5b085a6e943d8ac
tony942316 <grassoa159@gmail.com>
2024-03-10 11:08:31 +0000

    Moved Tests Out Of Functions

--- ProvingGrounds/TestStdm/TestContainers/TestArray.cpp
+++ ProvingGrounds/TestStdm/TestContainers/TestArray.cpp
@@ -2,84 +2,18 @@ export module TestArray;
 
 import Stdm.Containers.Array;
 
-export namespace Test::Array
+namespace Test::Array::CTT
 {
-    consteval void ImplicitMembers() noexcept
+    namespace Helpers
     {
-        constexpr auto arr1 = stdm::array<int, 3>();
-        static_assert(arr1[0] == 0 && arr1[1] == 0 && arr1[2] == 0);
-
-        constexpr auto arr2 = stdm::array<int, 3>({ 1, 2, 3 });
-        static_assert(arr2[0] == 1 && arr2[1] == 2 && arr2[2] == 3);
-
-        constexpr auto helper = []() consteval
+        constexpr auto CopyAssigned = []() consteval
             {
                 auto arr1 = stdm::array<int, 3>({ 3, 2, 1 });
                 auto arr2 = stdm::array<int, 3>();
                 arr2 = arr1;
                 return arr2;
             };
-        constexpr auto arr3 = helper();
-        static_assert(arr3[0] == 3 && arr3[1] == 2 && arr3[2] == 1);
-    }
-
-    consteval void ElementAccess() noexcept
-    {
-        constexpr auto arr = stdm::array<int, 3>({ 1, 2, 3 });
-
-        static_assert(arr.at(0) == 1 && arr.at(1) == 2 && arr.at(2) == 3);
-        static_assert(arr.front() == 1);
-        static_assert(arr.back() == 3);
-        static_assert(arr.data() == &arr.at(0) && *(arr.data() + 1) == 2);
-
-        static_assert(stdm::data(arr) == arr.data());
-    }
-
-    consteval void Iterators() noexcept
-    {
-        constexpr auto arr = stdm::array<int, 3>({ 1, 2, 3 });
-
-        static_assert(*arr.begin() == *arr.cbegin());
-        static_assert(*(arr.end() - 1) == *(arr.cend() - 1));
-        static_assert(*arr.rbegin() == *arr.crbegin());
-        static_assert(*(arr.rend() - 1) == *(arr.crend() - 1));
-
-        static_assert(*arr.rbegin() == *(arr.end() - 1));
-        static_assert(*arr.begin() == *(arr.rend() - 1));
-        static_assert(*arr.crbegin() == *(arr.cend() - 1));
-        static_assert(*arr.cbegin() == *(arr.crend() - 1));
-
-        static_assert(stdm::begin(arr) == arr.begin());
-        static_assert(stdm::cbegin(arr) == arr.cbegin());
-        static_assert(stdm::end(arr) == arr.end());
-        static_assert(stdm::cend(arr) == arr.cend());
-        static_assert(stdm::rbegin(arr) == arr.rbegin());
-        static_assert(stdm::crbegin(arr) == arr.crbegin());
-        static_assert(stdm::rend(arr) == arr.rend());
-        static_assert(stdm::crend(arr) == arr.crend());
-    }
-
-    consteval void Capacity() noexcept
-    {
-        constexpr auto arr = stdm::array<int, 3>({ 1, 2, 3 });
-        constexpr auto arrE = stdm::array<int, 0>();
-
-        static_assert(!arr.empty());
-        static_assert(arrE.empty());
-
-        static_assert(arr.size() == 3);
-
-        static_assert(arr.max_size() == arr.size());
-        static_assert(arrE.max_size() == arrE.size());
-
-        static_assert(stdm::empty(arr) == arr.empty());
-        static_assert(stdm::size(arr) == arr.size());
-        static_assert(stdm::ssize(arr) == arr.size());
-    }
-
-    consteval void Operations() noexcept
-    {
-        constexpr auto helper = []() consteval
+        constexpr auto FillSwap = []() consteval
             {
                 auto arr1 = stdm::array<int, 3>();
                 auto arr2 = stdm::array<int, 3>();
@@ -88,55 +22,105 @@ export namespace Test::Array
                 arr1.swap(arr2);
                 return arr1;
             };
-        static_assert(helper() == stdm::array<int, 3>({ 2, 2, 2 }));
-    }
-
-    consteval void NonMembers() noexcept
-    {
-        constexpr auto arr1 = stdm::array<int, 3>({ 1, 2, 3 });
-        constexpr auto arr2 = stdm::array<int, 3>({ 3, 2, 1 });
-        constexpr auto arr3 = arr1;
-
-        static_assert(arr1 == arr3);
-        static_assert(arr1 != arr2);
-
-        static_assert(arr1 < arr2);
-        static_assert(arr1 <= arr3);
-        static_assert(arr2 > arr1);
-        static_assert(arr1 >= arr3);
-
-        static_assert(stdm::get<0>(arr1) == 1 && stdm::get<1>(arr1) == 2 &&
-            stdm::get<2>(arr1) == 3);
-
-        constexpr auto helper = []() consteval
+        constexpr auto NMSwap = []() consteval
             {
                 auto arr1 = stdm::array<int, 3>({ 1, 2, 3 });
                 auto arr2 = stdm::array<int, 3>({ 5, 6, 7 });
                 stdm::swap(arr1, arr2);
                 return arr1;
             };
-        constexpr auto arr4 = helper();
-        static_assert(arr4 == stdm::array<int, 3>({ 5, 6, 7 }));
-
-        constexpr int arr5[] = { 4, 5, 6 };
-        static_assert(stdm::to_array(arr5) == stdm::array<int, 3>({ 4, 5, 6 }));
     }
 
-    consteval void Helpers() noexcept
+    namespace Arr
     {
-        constexpr auto arr = stdm::array<int, 3>({ 1, 2, 3 });
-        static_assert(stdm::tuple_size<stdm::array<int, 3>>() == 3);
-        using type = stdm::tuple_element<0, stdm::array<int, 3>>::type;
+        constexpr auto NoElements = stdm::array<int, 0>();
+        constexpr auto Empty = stdm::array<int, 3>();
+        constexpr auto Basic = stdm::array<int, 3>({ 1, 2, 3 });
+        constexpr auto Reverse = stdm::array<int, 3>({ 3, 2, 1 });
+        constexpr auto CA = Helpers::CopyAssigned();
+        constexpr auto FS = Helpers::FillSwap();
+        constexpr auto NMS = Helpers::NMSwap();
+        constexpr int CStyle[] = { 5, 6, 7 };
     }
 
-    consteval void Run() noexcept
+
+    // Implicit Members
+    static_assert(Arr::Empty[0] == 0 && Arr::Empty[1] == 0 &&
+        Arr::Empty[2] == 0);
+    static_assert(Arr::Basic[0] == 1 && Arr::Basic[1] == 2 &&
+        Arr::Basic[2] == 3);
+    static_assert(Arr::CA[0] == 3 && Arr::CA[1] == 2 && Arr::CA[2] == 1);
+
+    // Element Access
+    static_assert(Arr::Basic.at(0) == 1 && Arr::Basic.at(1) == 2 &&
+        Arr::Basic.at(2) == 3);
+    static_assert(Arr::Basic.front() == 1);
+    static_assert(Arr::Basic.back() == 3);
+    static_assert(Arr::Basic.data() == &Arr::Basic.at(0) &&
+        *(Arr::Basic.data() + 1) == 2);
+
+    static_assert(stdm::data(Arr::Basic) == Arr::Basic.data());
+
+    // Iterators
+    static_assert(*Arr::Basic.begin() == *Arr::Basic.cbegin());
+    static_assert(*(Arr::Basic.end() - 1) == *(Arr::Basic.cend() - 1));
+    static_assert(*Arr::Basic.rbegin() == *Arr::Basic.crbegin());
+    static_assert(*(Arr::Basic.rend() - 1) == *(Arr::Basic.crend() - 1));
+
+    static_assert(*Arr::Basic.rbegin() == *(Arr::Basic.end() - 1));
+    static_assert(*Arr::Basic.begin() == *(Arr::Basic.rend() - 1));
+    static_assert(*Arr::Basic.crbegin() == *(Arr::Basic.cend() - 1));
+    static_assert(*Arr::Basic.cbegin() == *(Arr::Basic.crend() - 1));
+
+    static_assert(stdm::begin(Arr::Basic) == Arr::Basic.begin());
+    static_assert(stdm::cbegin(Arr::Basic) == Arr::Basic.cbegin());
+    static_assert(stdm::end(Arr::Basic) == Arr::Basic.end());
+    static_assert(stdm::cend(Arr::Basic) == Arr::Basic.cend());
+    static_assert(stdm::rbegin(Arr::Basic) == Arr::Basic.rbegin());
+    static_assert(stdm::crbegin(Arr::Basic) == Arr::Basic.crbegin());
+    static_assert(stdm::rend(Arr::Basic) == Arr::Basic.rend());
+    static_assert(stdm::crend(Arr::Basic) == Arr::Basic.crend());
+
+    // Capacity
+    static_assert(!Arr::Basic.empty());
+    static_assert(Arr::NoElements.empty());
+
+    static_assert(Arr::Basic.size() == 3);
+
+    static_assert(Arr::Basic.max_size() == Arr::Basic.size());
+    static_assert(Arr::NoElements.max_size() == Arr::NoElements.size());
+
+    static_assert(stdm::empty(Arr::Basic) == Arr::Basic.empty());
+    static_assert(stdm::size(Arr::Basic) == Arr::Basic.size());
+    static_assert(stdm::ssize(Arr::Basic) == Arr::Basic.size());
+
+    // Operations
+    static_assert(Arr::FS == stdm::array<int, 3>({ 2, 2, 2 }));
+
+    // Non Members
+    static_assert(Arr::Basic == Arr::Basic);
+    static_assert(Arr::Basic != Arr::Reverse);
+
+    static_assert(Arr::Basic < Arr::Reverse);
+    static_assert(Arr::Basic <= Arr::Basic);
+    static_assert(Arr::Reverse > Arr::Basic);
+    static_assert(Arr::Basic >= Arr::Basic);
+
+    static_assert(stdm::get<0>(Arr::Basic) == 1 &&
+        stdm::get<1>(Arr::Basic) == 2 && stdm::get<2>(Arr::Basic) == 3);
+
+    static_assert(Arr::NMS == stdm::array<int, 3>({ 5, 6, 7 }));
+    static_assert(stdm::to_array(Arr::CStyle) == Arr::NMS);
+
+    // Helpers
+    static_assert(stdm::tuple_size<stdm::array<int, 3>>() == 3);
+    using type = stdm::tuple_element<0, stdm::array<int, 3>>::type;
+}
+
+export namespace Test::Array
+{
+    void Run() noexcept
     {
-        ImplicitMembers();
-        ElementAccess();
-        Iterators();
-        Capacity();
-        Operations();
-        NonMembers();
-        Helpers();
+
     }
 }
