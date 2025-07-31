0e7f49c496f6fc6f80f719348a4397171bbe3f11
tony942316 <grassoa159@gmail.com>
2025-07-31 07:54:27 -0400

    Version 0.10 First

---
+++ Tests/Basic.cpp
@@ -0,0 +1,13 @@
+import <Eqx/std.hpp>;
+
+using namespace std::literals;
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    std::format_to(std::ostreambuf_iterator<char>(std::cout), "Start\n\n"sv);
+
+
+
+    std::format_to(std::ostreambuf_iterator<char>(std::cout), "\nEnd\n"sv);
+    return EXIT_SUCCESS;
+}
